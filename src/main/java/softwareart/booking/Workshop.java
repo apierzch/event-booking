cc8f00cc0eda67e1c5b2398df6948f18d3b5205f
Adam Pierzchała <pierzchala.adam@gmail.com>
2013-09-21 12:29:39 +0200

    maven build

--- src/main/java/softwareart/booking/Workshop.java
+++ src/main/java/softwareart/booking/Workshop.java
@@ -16,20 +16,28 @@ public class Workshop {
         this.end = end;
     }
 
+    public boolean collidesWith(Workshop workshop) {
+        return startsDuring(workshop) || workshop.startsDuring(this);
+    }
+
+    private boolean startsDuring(Workshop workshop) {
+        return this.start >= workshop.start && this.start <= workshop.end;
+    }
+
     public String getTitle() {
         return title;
     }
 
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
     public Collection<String> getParticipants() {
         return participants;
-    }
-
-    public boolean collidesWith(Workshop workshop) {
-        return startDuring(workshop) || workshop.startDuring(this);
-    }
-
-    private boolean startDuring(Workshop workshop) {
-        return this.start >= workshop.start && this.start <= workshop.end;
     }
 
 }
