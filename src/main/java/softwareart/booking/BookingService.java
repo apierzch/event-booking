ae920c41b7429d13901efc102981f0ae0ca68dc9
Adam Pierzchała <pierzchala.adam@gmail.com>
2013-09-22 14:48:17 +0200

    listing workshops + booking for many at a time

--- src/main/java/softwareart/booking/BookingService.java
+++ src/main/java/softwareart/booking/BookingService.java
@@ -10,18 +10,46 @@ public class BookingService {
 
     private Map<String, Workshop> workshops = new HashMap<String, Workshop>();
 
-    public void book(String workshopTitle, String participantMail) {
-        Collection<Workshop> collidingWorkshops = getCollidingWorkshops(getWorkshop(workshopTitle));
-        for (Workshop workshop : collidingWorkshops) {
-            if(workshop.getParticipants().contains(participantMail)) {
-                throw new IllegalStateException("Colliding workshops selected");
+    public void book(String participantMail, String... workshopTitles) {
+        Workshop[] workshops = getWorkshops(workshopTitles);
+
+        verifyCollisions(workshops);
+        removeBookingFor(participantMail);
+        makeBooking(participantMail, workshops);
+    }
+
+    private void verifyCollisions(Workshop[] workshops) {
+        for (Workshop workshop : workshops) {
+            for (Workshop testWorkshop : workshops) {
+                if (testWorkshop != workshop && workshop.collidesWith(testWorkshop)) {
+                    throw new IllegalArgumentException("Colliding workshops selected");
+                }
             }
         }
-        getParticipantsAt(workshopTitle).add(participantMail);
+    }
+
+    private void makeBooking(String participantMail, Workshop[] workshops) {
+        for (Workshop workshop : workshops) {
+            workshop.getParticipants().add(participantMail);
+        }
+    }
+
+    private void removeBookingFor(String participantMail) {
+        for (Workshop workshop : this.workshops.values()) {
+            workshop.getParticipants().remove(participantMail);
+        }
+    }
+
+    private Workshop[] getWorkshops(String[] workshopTitles) {
+        Workshop[] workshops = new Workshop[workshopTitles.length];
+        for (int i = 0; i < workshops.length; i++) {
+            workshops[i] = getWorkshop(workshopTitles[i]);
+        }
+        return workshops;
     }
 
     private Workshop getWorkshop(String workshopTitle) {
-        if(!workshops.containsKey(workshopTitle)) {
+        if (!workshops.containsKey(workshopTitle)) {
             throw new IllegalArgumentException("No such workshop");
         }
         return workshops.get(workshopTitle);
@@ -31,17 +59,17 @@ public class BookingService {
         return getWorkshop(title).getParticipants();
     }
 
-    private Collection<Workshop> getCollidingWorkshops(Workshop workshop) {
-        LinkedList<Workshop> result = new LinkedList<Workshop>();
-        for (Workshop element : workshops.values()) {
-            if(workshop.collidesWith(element)) {
-                result.add(element);
+    public void addWorkshop(Workshop workshop) {
+        workshops.put(workshop.getTitle(), workshop);
+    }
+
+    public Collection<Workshop> getWorkshopsStartingAtSlot(int slot) {
+        Collection<Workshop> result = new LinkedList<Workshop>();
+        for (Workshop workshop : workshops.values()) {
+            if (workshop.getStart() == slot) {
+                result.add(workshop);
             }
         }
         return result;
-    }
-
-    public void addWorkshop(Workshop workshop) {
-        workshops.put(workshop.getTitle(), workshop);
     }
 }
--- src/test/java/softwareart/booking/BookingServiceTest.java
+++ src/test/java/softwareart/booking/BookingServiceTest.java
@@ -7,7 +7,9 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.fest.assertions.Assertions.*;
+import java.util.Collection;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class BookingServiceTest {
@@ -19,7 +21,7 @@ public class BookingServiceTest {
         service.addWorkshop(new Workshop("workshop", 1, 1));
 
         // when
-        service.book("workshop", "test@test.com");
+        service.book("test@test.com", "workshop");
 
         // then
         assertThat(service.getParticipantsAt("workshop")).containsOnly("test@test.com");
@@ -37,30 +39,69 @@ public class BookingServiceTest {
         service.addWorkshop(new Workshop("workshop", 1, 1));
 
         // when
-        CatchExceptionBdd.when(service).book("invalidWorkshop", "test@test.com");
+        CatchExceptionBdd.when(service).book("test@test.com", "invalidWorkshop");
 
         // then
         CatchExceptionBdd.then(CatchException.caughtException())
-                         .isInstanceOf(IllegalArgumentException.class)
-                         .hasMessage("No such workshop");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("No such workshop");
     }
 
     @Test
     @Parameters({"1,2, 1,3",
-                 "1,3, 2,3",
-                 "1,3, 2,2",
-                 "2,2, 1,3"})
+            "1,3, 2,3",
+            "1,3, 2,2",
+            "2,2, 1,3"})
     public void shouldNotAllowBookingForCollidingWorkshops(int firstStart, int firstEnd, int secondStart, int secondEnd) {
         service.addWorkshop(new Workshop("workshop1", firstStart, firstEnd));
         service.addWorkshop(new Workshop("workshop2", secondStart, secondEnd));
 
         // when
-        service.book("workshop1", "test@test.com");
-        CatchExceptionBdd.when(service).book("workshop2", "test@test.com");
+        CatchExceptionBdd.when(service).book("test@test.com", "workshop1", "workshop2");
 
         // then
         CatchExceptionBdd.then(CatchException.caughtException())
-                         .isInstanceOf(IllegalStateException.class)
-                         .hasMessage("Colliding workshops selected");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Colliding workshops selected");
     }
+
+    @Test
+    public void shouldListCorrectSubset() {
+        Workshop workshop1 = new Workshop("workshop1", 1, 1);
+        service.addWorkshop(workshop1);
+        Workshop workshop2 = new Workshop("workshop2", 2, 2);
+        service.addWorkshop(workshop2);
+        Workshop workshop3 = new Workshop("workshop3", 3, 3);
+        service.addWorkshop(workshop3);
+        Workshop workshop4 = new Workshop("workshop4", 1, 2);
+        service.addWorkshop(workshop4);
+        Workshop workshop5 = new Workshop("workshop5", 2, 3);
+        service.addWorkshop(workshop5);
+        Workshop workshop6 = new Workshop("workshop6", 1, 3);
+        service.addWorkshop(workshop6);
+
+        // when
+        Collection<Workshop> workshopsAt1 = service.getWorkshopsStartingAtSlot(1);
+        Collection<Workshop> workshopsAt2 = service.getWorkshopsStartingAtSlot(2);
+        Collection<Workshop> workshopsAt3 = service.getWorkshopsStartingAtSlot(3);
+
+        // then
+        assertThat(workshopsAt1).containsOnly(workshop1, workshop4, workshop6);
+        assertThat(workshopsAt2).containsOnly(workshop2, workshop5);
+        assertThat(workshopsAt3).containsOnly(workshop3);
+    }
+
+    @Test
+    public void shouldRemoveParticipantFromPreviousBooking() {
+        service.addWorkshop(new Workshop("workshop1", 1, 1));
+        service.addWorkshop(new Workshop("workshop2", 2, 2));
+        service.book("test@test.com", "workshop1");
+
+        // when
+        service.book("test@test.com", "workshop2");
+
+        assertThat(service.getParticipantsAt("workshop1")).isEmpty();
+        assertThat(service.getParticipantsAt("workshop2")).containsOnly("test@test.com");
+    }
+
 }
